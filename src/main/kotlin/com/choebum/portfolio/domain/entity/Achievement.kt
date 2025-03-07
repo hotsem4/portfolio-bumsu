bbdcea8ee0b8c7477bbc2a630af05ef28230875a
Bumsu Choe <hotsem100@gmail.com>
2025-03-07 16:49:59 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/choebum/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/choebum/portfolio/domain/entity/Achievement.kt
@@ -1,11 +1,29 @@
 package com.choebum.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(  // 기본 생성자 생성을 이렇게 하는구나... 와우
+    title: String,
+    description: String,
+    achievedDate: LocalDate?,
+    host: String,
+    isActive: Boolean
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var achievedDate: LocalDate? = achievedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
+
 }
--- src/main/kotlin/com/choebum/portfolio/domain/entity/HttpInterface.kt
+++ src/main/kotlin/com/choebum/portfolio/domain/entity/HttpInterface.kt
@@ -1,11 +1,32 @@
 package com.choebum.portfolio.domain.entity
 
 import jakarta.persistence.*
+import jakarta.servlet.http.HttpServletRequest
 
 @Entity
-class HttpInterface : BaseEntity() {
+class HttpInterface(
+    httpServletRequest: HttpServletRequest,
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "http_interface_id")
     var id: Long? = null
+
+    var cookies: String? = httpServletRequest.cookies
+        ?.map { "${it.name}: ${it.value}" }
+        ?.toString()
+
+    var referer: String? =
+        httpServletRequest.getHeader("referer") // 특정 사이트를 검색해서 들어가게 될 경우 특정 사이트는 우리의 요청을 듣고 응답을 해주는데 그때 요청이 어디서 부터 왔는지 판단
+
+    var localAddr: String? = httpServletRequest.localAddr
+
+    var remoteAddr: String? = httpServletRequest.remoteAddr
+
+    var remoteHost: String? = httpServletRequest.remoteHost
+
+    var requestUri: String? = httpServletRequest.requestURI
+
+    var userAgent: String? = httpServletRequest.getHeader("User-Agent")  // 브라우저 정보
+
 }
