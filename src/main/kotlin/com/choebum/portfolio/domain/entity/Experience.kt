43cdc0fcd9924054bde0f8cd3c9b84239af38302
Bumsu Choe <hotsem100@gmail.com>
2025-03-07 17:29:34 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/choebum/portfolio/domain/entity/Experience.kt
+++ src/main/kotlin/com/choebum/portfolio/domain/entity/Experience.kt
@@ -3,9 +3,72 @@ package com.choebum.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Experience : BaseEntity() {
+class Experience(
+    title: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean,
+) : BaseEntity() {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(
+        targetEntity = ExperienceDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL],
+    )
+    @JoinColumn(name = "experience_id")
+    var details: MutableList<ExperienceDetail> = mutableListOf()
+    // List<ExperienceDetail> = {} 이라는 빈 리스트를 만든다.
+
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+        return "${endYear}.${endMonth}" // 2025.11 이렇게 문자열이 나가게 된다.
+    }
+
+    fun update(
+        title: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean,
+    ) {
+        this.title = title
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(detail: MutableList<ExperienceDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
--- src/main/kotlin/com/choebum/portfolio/domain/entity/Project.kt
+++ src/main/kotlin/com/choebum/portfolio/domain/entity/Project.kt
@@ -3,9 +3,78 @@ package com.choebum.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Project : BaseEntity() {
+class Project(
+    name: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean,
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
     var id: Long? = null
+
+
+    var name: String = name
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(
+        targetEntity = ProjectDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL],
+    )
+    @JoinColumn(name = "project_id")
+    var details: MutableList<ProjectDetail> = mutableListOf()
+
+
+    @OneToMany(
+        targetEntity = ProjectSkill::class,
+        mappedBy = "project",
+    )
+    var skills: MutableList<Skill> = mutableListOf()
+
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+        return "${endYear}.${endMonth}" // 2025.11 이렇게 문자열이 나가게 된다.
+    }
+
+    fun update(
+        name: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean,
+    ) {
+        this.name = name
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(detail: MutableList<ProjectDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
