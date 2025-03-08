6199b3f896735686de0e3cb0a1d20917842c7775
Bumsu Choe <hotsem100@gmail.com>
2025-03-08 15:32:50 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/choebum/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/choebum/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,12 +2,17 @@ package com.choebum.portfolio.domain.repository
 
 import com.choebum.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
+
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
 
 
-//    override fun findById(id: Long): Optional<Experience>
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
+    override fun findById(id: Long): Optional<Experience>
     /**
      * 상위에 정의되어 있는 findById를 왜 override할까?
      * 그 이유는 Experience가 1:다 매핑이되어 있기 때문에 기존 정의된 그대로 사용하게 될 경우
