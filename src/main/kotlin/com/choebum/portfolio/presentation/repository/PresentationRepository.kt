a18a0b9d7e6906a8a887c59b15254b27839325b4
Bumsu Choe <hotsem100@gmail.com>
2025-03-12 13:53:15 +0900

    리포지토리 개발

--- src/main/kotlin/com/choebum/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/choebum/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,6 +1,40 @@
 package com.choebum.portfolio.presentation.repository
 
+import com.choebum.portfolio.domain.entity.*
+import com.choebum.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val skillRepository: SkillRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val experienceRepository: ExperienceRepository,
+    private val projectRepository: ProjectRepository,
+) {
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+
+}
