06aa16ecb4e893b8c4d7af6b3bfd9ec1ba1ef598
Bumsu Choe <hotsem100@gmail.com>
2025-03-15 15:17:20 +0900

    Intercepter 개발

--- src/main/kotlin/com/choebum/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/choebum/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,23 @@
 package com.choebum.portfolio.presentation.interceptor
 
+import com.choebum.portfolio.domain.entity.HttpInterface
+import com.choebum.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
